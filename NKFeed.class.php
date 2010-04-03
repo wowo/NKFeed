b985f31493733bbc35add5c3d6f125d7cebf1c8b
Wojciech Sznapka <wojciech@sznapka.pl>
2010-04-03 23:17:36 +0200

    fixed a typo

--- NKFeed.class.php
+++ NKFeed.class.php
@@ -46,7 +46,7 @@ class NKFeed
         CURLOPT_COOKIEJAR  => "/tmp/nk.cookie",
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_POSTFIELDS => $data,
-        CURLOPT_REFERER => "http://nasza-klasa.pl/"
+        CURLOPT_REFERER => "http://nasza-klasa.pl/",
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_URL => "http://nasza-klasa.pl/login",
         CURLOPT_USERAGENT  => "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1",
