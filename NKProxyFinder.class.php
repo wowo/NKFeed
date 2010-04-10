9e930640f44e8079c8da53083ee7ec2953f277d1
Wojciech Sznapka <wojciech@sznapka.pl>
2010-04-10 12:01:50 +0200

    Documentation improvements

--- NKProxyFinder.class.php
+++ NKProxyFinder.class.php
@@ -1,13 +1,29 @@
 <?php
 include_once('NKException.class.php');
 
+/**
+ * Finds proxy to connect to nasza-klasa.pl and retrieves valid proxies (those with you can connect to nasza-klasa.pl) from proxy list
+ * 
+ * @package default
+ * @version $id$
+ * @copyright 
+ * @author Wojciech Sznapka <wojciech@sznapka.pl> 
+ * @license 
+ */
 class NKProxyFinder
 {
   private $csvFilepath = '';
 
-  const PROXY_IP = 0;
+  const PROXY_IP   = 0;
   const PROXY_PORT = 1;
 
+  /**
+   * The constructor
+   * 
+   * @param mixed $filepath 
+   * @access public
+   * @return void
+   */
   public function NKProxyFinder($filepath)
   {
     if (!is_readable($filepath)) {
@@ -16,6 +32,12 @@ class NKProxyFinder
     $this->csvFilepath = $filepath;
   }
 
+  /**
+   * gets proxies with which there is ability to connect to nasza-klasa.pl.  Proxies are taken from $this->csvFilepath
+   * 
+   * @access public
+   * @return array
+   */
   public function getValidProxies()
   {
     $proxies = array();
@@ -28,6 +50,12 @@ class NKProxyFinder
     return $proxies;
   }
 
+  /**
+   * get one random proxy from $this->csvFilepath, as an assoc array: array(ip => <string>, port => <int>)
+   * 
+   * @access public
+   * @return array
+   */
   public function getRandomProxy()
   {
     $proxies = $this->readCSV($this->csvFilepath);
@@ -37,6 +65,14 @@ class NKProxyFinder
     return $this->getProxyFromRecord($proxies[array_rand($proxies)]);
   }
 
+  /**
+   * saves valid proxies to file 
+   * 
+   * @see NKProxyFinder::getValidProxies
+   * @param string $filepath 
+   * @access public
+   * @return void
+   */
   public function saveValidProxiesToFile($filepath = 'data/proxies.csv')
   {
     if (!is_writable(dirname($filepath))) {
@@ -50,12 +86,58 @@ class NKProxyFinder
     file_put_contents($filepath, implode("\n", $output));
   }
 
+  /**
+   * get proxy as an array from csv record 
+   * 
+   * @param mixed $record 
+   * @access private
+   * @return array(ip => <string>, port => <int>)
+   */
   private function getProxyFromRecord($record)
   {
     return array('ip' => $record[self::PROXY_IP], 'port' => $record[self::PROXY_PORT]);
   }
 
+  /**
+   * checks is proxy is valid - not banned on nasza-klasa.pl's firewall.
+   * It simply curls nasza-klasa with 3 second timeout and check if there's no error and response code is 200
+   * 
+   * @param mixed $ip 
+   * @param mixed $port 
+   * @access private
+   * @return boolean
+   */
+  private function isValidProxy($ip, $port)
+  {
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_REFERER => "http://nasza-klasa.pl",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_URL => "http://nasza-klasa.pl",
+      CURLOPT_USERAGENT  => "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1",
+      CURLOPT_PROXY => $ip,
+      CURLOPT_PROXYPORT => $port,
+      CURLOPT_TIMEOUT => 3,
+      )
+    );
+    $html = curl_exec($curl);
+    if ($error = curl_error($curl)) {
+      return false;
+    }
+    if (($code = curl_getinfo($curl, CURLINFO_HTTP_CODE)) != 200) {
+      return false;
+    }
+    curl_close($curl);
+    return true;
+  }
 
+  /**
+   * reads CSV and returns it as an array
+   * 
+   * @param mixed $filepath 
+   * @access private
+   * @return array
+   */
   private function readCSV ($filepath)
   {
     $csv = file($filepath);
@@ -63,29 +145,5 @@ class NKProxyFinder
       $record = explode(';', $record);
     }
     return $csv;
-  }
-
-  private function isValidProxy($ip, $port)
-  {
-      $curl = curl_init();
-      curl_setopt_array($curl, array(
-        CURLOPT_REFERER => "http://nasza-klasa.pl",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_URL => "http://nasza-klasa.pl",
-        CURLOPT_USERAGENT  => "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1",
-        CURLOPT_PROXY => $ip,
-        CURLOPT_PROXYPORT => $port,
-        CURLOPT_TIMEOUT => 3,
-        )
-      );
-      $html = curl_exec($curl);
-      if ($error = curl_error($curl)) {
-        return false;
-      }
-      if (($code = curl_getinfo($curl, CURLINFO_HTTP_CODE)) != 200) {
-        return false;
-      }
-      curl_close($curl);
-      return true;
   }
 }
